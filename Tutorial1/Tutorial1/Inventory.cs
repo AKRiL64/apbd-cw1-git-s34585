7c23e559f665ac6555d15fd37937eb78f2f1dee7
Kyryl <lerroy.k@icloud.com>
2026-03-10 16:47:16 +0100

    Now there's functionality for this classes

--- Tutorial1/Tutorial1/Inventory.cs
+++ Tutorial1/Tutorial1/Inventory.cs
@@ -2,5 +2,18 @@
 
 public class Inventory
 {
-    
+    private ValuableItem[] items;
+    int amount = 0;
+
+    Inventory()
+    {
+        items = new ValuableItem[10];
+    }
+
+    public void AddItem(ValuableItem item)
+    {
+        if (amount >= items.Length - 1) throw new Exception();
+        items[amount] = item;
+        amount++;
+    }
 }
--- Tutorial1/Tutorial1/ValuableItem.cs
+++ Tutorial1/Tutorial1/ValuableItem.cs
@@ -2,5 +2,10 @@
 
 public class ValuableItem
 {
-    
+    public int value {private set; get;}
+
+    ValuableItem(int value)
+    {
+        value = value;
+    }
 }
