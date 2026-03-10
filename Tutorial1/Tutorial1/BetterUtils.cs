d286ba893483a9235d4733421da6a3e5ab4e9bbf
Kyryl <lerroy.k@icloud.com>
2026-03-10 17:01:52 +0100

    New BETTER Utils based on old ones

---
+++ Tutorial1/Tutorial1/BetterUtils.cs
@@ -0,0 +1,9 @@
+﻿namespace Tutorial1;
+
+public class BetterUtils: Utils
+{
+    public static ValuableItem GetCheaperItem(ValuableItem item1, ValuableItem item2)
+    {
+        return item1.value <= item2.value? item1:item2;
+    }
+}
