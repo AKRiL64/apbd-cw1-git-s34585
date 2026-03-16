f85cb59db79077dc32f25f69340c429f0d9cf732
Kiryu Setsuna <lerroy.k@icloud.com>
2026-03-16 19:03:47 +0100

    In Program.cs, added sample code for testing items and inventory

--- Tutorial1/Tutorial1/Program.cs
+++ Tutorial1/Tutorial1/Program.cs
@@ -1,3 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
-
-Console.WriteLine("Hello, World!");
+namespace Tutorial1;
+public class Program {
+    public static void Main(string[] args) {
+        Console.WriteLine("Hello, I'm working!");
+        ValuableItem item = new ValuableItem(10);
+        ValuableItem item2 = new ValuableItem(20);
+        ValuableItem item3 = Utils.AddTwoItems(item, item2);
+        Console.WriteLine(item3.value);
+        Inventory inv = new Inventory();
+        inv.AddItem(item);
+        inv.AddItem(item2);
+        inv.AddItem(item3);
+        Console.WriteLine(inv.GetMaxItem().value);
+        Console.WriteLine(inv.GetMinItem().value);
+    }
+}
